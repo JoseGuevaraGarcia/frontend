178f802a25903204f95ded49aabdf527989373c4
Jose Guevara <guevarajose709@gmail.com>
2022-02-28 00:52:45 -0300

    migrando a react

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = (props) => {
     return (
         <footer>
-            <p>Derechos reservados. Transporte X año</p>
+            <p>Derechos reservados. Sabor Venezolano año</p>
         </footer>
     );
 }
